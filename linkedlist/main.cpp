dc8c65df308c96f7e0ecaf2fbed09a6ae8e0afb5
sarthak shah <sarthak@nbp-94-67.nbp.ruw.rutgers.edu>
2014-09-23 14:47:51 -0400

    Initial Commit

---
+++ linkedlist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  linkedlist
+//
+//  Created by sarthak shah on 9/23/14.
+//  Copyright (c) 2014 SarthakWorks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
